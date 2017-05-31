39b8fef2621457cb08afadb0ecf927dbd4118bfc
llopez32 <llopez32@gatech.edu>
2017-05-31 16:18:55 -0400

    added script

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'trappedionsqsim',
+    'description': 'Quantum and Classical Simulations for Trapped Ions',
     'author': 'Omid Khosravani',
     'url': 'https://github.com/trxw/trappedionsqsim',
     'version': '0.1',
     'install_requires': ['qutip'],
     'packages': ['trappedionsqsim',],
+    'scripts': ['bin/trappedionsqsim'],
     'name': 'trappedionsqsim'
 }
 
