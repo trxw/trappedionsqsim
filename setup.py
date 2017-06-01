1810c43cd624919fafa25db5fdd3c29a47997e28
llopez32 <llopez32@gatech.edu>
2017-06-01 15:42:23 -0400

    finalized packaging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 config = {
     'description': 'Quantum and Classical Simulations for Trapped Ions',
-    'author': 'Omid Khosravani',
+    'author': 'Omid Khosravani, Lauren Lopez',
     'url': 'https://github.com/trxw/trappedionsqsim',
     'version': '0.1',
     'install_requires': ['qutip', 'numpy', 'matplotlib', 'scipy'],
