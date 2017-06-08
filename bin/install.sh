49d0b482b64f0e09db36b41b87fcc3eda7f6386f
Omid Khosravani <okhosravani@gmail.com>
2017-06-07 22:37:10 -0400

    cleanup

--- bin/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-sudo apt-get install python-numpy
-sudo apt-get install python-scipy
-sudo apt-get install python-qutip
-sudo apt-get install python-matplotlib
-python setup.py install
