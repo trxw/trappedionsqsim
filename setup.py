02bc1787d81e4b54f6493c24add96ebc08715136
trxw <okhosravani@gmail.com>
2019-08-11 20:43:53 -0400

    restructure package, debug setup, operators and simulations modules

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
+from setuptools import setup, find_packages
+import sys, os.path
+
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 config = {
+    'name': 'trappedionsqsim',
     'description': 'Quantum and Classical Simulations for Trapped Ions',
-    'author': 'Omid Khosravani, LDaniel Murphy, Lauren Lopez',
+    'author': 'Omid Khosravani, Daniel Murphy, Lauren Lopez',
     'url': 'https://github.com/trxw/trappedionsqsim',
     'version': '0.1',
     'install_requires': ['qutip', 'numpy', 'matplotlib', 'scipy'],
-    'packages': ['utils',],
-    'name': 'trappedionsqsim'
+    'packages': ['trappedionsqsim', 'trappedionsqsim.utils']
 }
 
 setup(**config)
